827c12f73c21257a217acaa827146b87bcfb70fc
RealToddler <emilien.jolivet@gmail.com>
2024-10-15 22:43:04 +0200

    still some improvements

---
+++ src/NeuralNetwork/XNOR.c
@@ -0,0 +1,191 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// neural network that can learn XOR function
+
+void softmax(double *input, double *output, int length) {
+    double max = input[0];
+    for (int i = 1; i < length; i++) {
+        if (input[i] > max)
+            max = input[i];
+    }
+
+    double sum = 0.0;
+    for (int i = 0; i < length; i++) {
+        output[i] = exp(input[i] - max);
+        sum += output[i];
+    }
+
+    for (int i = 0; i < length; i++) {
+        output[i] /= sum;
+    }
+}
+
+double init_weights() {
+    return ((double)rand()) / ((double)RAND_MAX);
+}
+
+void shuffle(int *array, size_t n) {
+    if (n > 1) {
+        size_t i;
+        for (i = 0; i < n - 1; i++) {
+            size_t j = i + (rand() / (RAND_MAX / (n - i) + 1));
+            int t = array[j];
+            array[j] = array[i];
+            array[i] = t;
+        }
+    }
+}
+
+#define INPUTS_NUMBER 2
+#define HIDDEN_NODES_NUMBER 2
+#define OUTPUTS_NUMBER 2
+#define TRAINING_SETS_NUMBER 4
+
+int main(void) {
+    const double lr = 0.1f;
+
+    double hidden_layer[HIDDEN_NODES_NUMBER];
+    double output_layer[OUTPUTS_NUMBER];
+
+    double hidden_layer_bias[HIDDEN_NODES_NUMBER];
+    double output_layer_bias[OUTPUTS_NUMBER];
+
+    double hidden_weights[INPUTS_NUMBER][HIDDEN_NODES_NUMBER];
+    double output_weights[HIDDEN_NODES_NUMBER][OUTPUTS_NUMBER];
+
+    double training_inputs[TRAINING_SETS_NUMBER][INPUTS_NUMBER] = {
+        {0.0f, 0.0f}, {1.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 1.0f}};
+
+    double training_outputs[TRAINING_SETS_NUMBER][OUTPUTS_NUMBER] = {
+        {1.0f, 0.0f}, {0.0f, 1.0f}, {0.0f, 1.0f}, {1.0f, 0.0f}};
+
+    for (int i = 0; i < INPUTS_NUMBER; i++) {
+        for (int j = 0; j < HIDDEN_NODES_NUMBER; j++) {
+            hidden_weights[i][j] = init_weights();
+        }
+    }
+
+    for (int i = 0; i < HIDDEN_NODES_NUMBER; i++) {
+        hidden_layer_bias[i] = init_weights();
+        for (int j = 0; j < OUTPUTS_NUMBER; j++) {
+            output_weights[i][j] = init_weights();
+        }
+    }
+
+    for (int i = 0; i < OUTPUTS_NUMBER; i++) {
+        output_layer_bias[i] = init_weights();
+    }
+
+    int training_set_order[] = {0, 1, 2, 3};
+
+    int number_of_epochs = 1000000;
+
+    // train the neural network for a number of epochs
+    for (int epoch = 0; epoch < number_of_epochs; epoch++) {
+        shuffle(training_set_order, TRAINING_SETS_NUMBER);
+
+        for (int x = 0; x < TRAINING_SETS_NUMBER; x++) {
+            int i = training_set_order[x];
+
+            // Forward pass
+
+            // Compute hidden layers activation
+            for (int j = 0; j < HIDDEN_NODES_NUMBER; j++) {
+                double activation = hidden_layer_bias[j];
+                for (int k = 0; k < INPUTS_NUMBER; k++) {
+                    activation += training_inputs[i][k] * hidden_weights[k][j];
+                }
+                hidden_layer[j] = fmax(0.0, activation);
+            }
+
+            // Compute output layers activation
+            double output_layer_input[OUTPUTS_NUMBER];
+            for (int j = 0; j < OUTPUTS_NUMBER; j++) {
+                double activation = output_layer_bias[j];
+                for (int k = 0; k < HIDDEN_NODES_NUMBER; k++) {
+                    activation += hidden_layer[k] * output_weights[k][j];
+                }
+                output_layer_input[j] = activation;
+            }
+
+            softmax(output_layer_input, output_layer, OUTPUTS_NUMBER);
+
+            // Back propagation
+            // Compute change in output weights
+
+            double delta_output[OUTPUTS_NUMBER];
+
+            for (int j = 0; j < OUTPUTS_NUMBER; j++) {
+                double error = output_layer[j] - training_outputs[i][j];
+                delta_output[j] = error;
+            }
+
+            // Compute change in hidden weights
+            double delta_hidden[HIDDEN_NODES_NUMBER];
+            for (int j = 0; j < HIDDEN_NODES_NUMBER; j++) {
+                double error = 0.0f;
+                for (int k = 0; k < OUTPUTS_NUMBER; k++) {
+                    error += delta_output[k] * output_weights[j][k];
+                }
+                delta_hidden[j] = error * (hidden_layer[j] > 0 ? 1.0 : 0.0);
+            }
+
+            // Apply changes in output weights
+            for (int j = 0; j < OUTPUTS_NUMBER; j++) {
+                output_layer_bias[j] -= delta_output[j] * lr;
+                for (int k = 0; k < HIDDEN_NODES_NUMBER; k++) {
+                    output_weights[k][j] -=
+                        hidden_layer[k] * delta_output[j] * lr;
+                }
+            }
+
+            // Apply changes in hidden weights
+            for (int j = 0; j < HIDDEN_NODES_NUMBER; j++) {
+                hidden_layer_bias[j] -= delta_hidden[j] * lr;
+                for (int k = 0; k < INPUTS_NUMBER; k++) {
+                    hidden_weights[k][j] -=
+                        training_inputs[i][k] * delta_hidden[j] * lr;
+                }
+            }
+        }
+
+        if ((epoch + 1) % 100000 == 0) {
+            printf("Epoch %d/%d completed\n", epoch + 1, number_of_epochs);
+        }
+    }
+
+    printf("Training completed after %d epochs.\n", number_of_epochs);
+
+    while (1) {
+        double input1, input2;
+        printf("Enter two inputs between 0 and 1 (or -1 to exit): ");
+        if (scanf("%lf %lf", &input1, &input2) != 2 || input1 == -1 ||
+            input2 == -1) {
+            break;
+        }
+
+        for (int j = 0; j < HIDDEN_NODES_NUMBER; j++) {
+            double activation = hidden_layer_bias[j];
+            activation += input1 * hidden_weights[0][j];
+            activation += input2 * hidden_weights[1][j];
+            hidden_layer[j] = fmax(0.0, activation);
+        }
+
+        double output_layer_input[OUTPUTS_NUMBER];
+        for (int j = 0; j < OUTPUTS_NUMBER; j++) {
+            double activation = output_layer_bias[j];
+            for (int k = 0; k < HIDDEN_NODES_NUMBER; k++) {
+                activation += hidden_layer[k] * output_weights[k][j];
+            }
+            output_layer_input[j] = activation;
+        }
+
+        softmax(output_layer_input, output_layer, OUTPUTS_NUMBER);
+
+        printf("Output : %d\n", output_layer[1] > output_layer[0] ? 1 : 0);
+    }
+
+    return 0;
+}
--- src/main.c
+++ src/main.c
@@ -29,50 +29,72 @@ int main() {
         return -1;
     }
 
-    int choice;
+    const char *path_to_preprocess[8] = {
+        "test_data/preprocess_1_1.png",
+        "test_data/preprocess_1_2.png",
+        "test_data/preprocess_2_1.png",
+        "test_data/preprocess_2_2.png",
+        "test_data/preprocess_3_1.png",
+        "test_data/preprocess_3_2.png",
+        "test_data/preprocess_4_1.png",
+        "test_data/preprocess_4_2.png",
+    };
+
+    const char *path_to_rotation = "test_data/rotation_2_2.png";
+
+        int choice;
 
     do {
         printf("======== OCR-NOUPI ========\n");
         printf("[ 1 ] Image Preprocessing.\n");
         printf("[ 2 ] Manual Rotation.\n");
         printf("[ 3 ] Grid Detection.\n");
-        printf("[ 4 ] Words List Detection.\n")
+        printf("[ 4 ] Words List Detection.\n");
         printf("[ 5 ] Words Detection.\n");
         printf("[ 6 ] Letters Extraction.\n");
         printf("[ 7 ] NeuralNetwork - Proof of Concept (XNOR).\n");
-        printf("[ 8 ] Solver.\n")
+        printf("[ 8 ] Solver.\n");
         printf("[ 9 ]  Quit.\n");
         scanf("%d", &choice);
 
         switch (choice) {
         case 1:
             printf("You chose  'Image Preprocessing'\n");
-            
-            iImage *img = resize_image(load_image(argv[1], -1), 1000, 1000);
 
-            if (img == NULL) {
-                printf("An error occured while loading the image");
-                free(img);
-                IMG_Quit();
-                SDL_Quit();
-                return EXIT_FAILURE;
+            for (int i = 1; i < 8; i++) {
+                for (int j = 1; j < 3; i++) {
+                    iImage *img =
+                        resize_image(load_image(path_to_images[i], -1), 1000, 1000);
+
+                    if (img == NULL) {
+                        printf("An error occured while loading the image");
+                        free(img);
+                        IMG_Quit();
+                        SDL_Quit();
+                        return EXIT_FAILURE;
+                    }
+
+                    grayscale(img);
+                    apply_gaussian_blur(img);
+                    increase_contrast(img, 10, 8);
+                    binary(img);
+                    otsu_threshold(img, 32);
+                    invert_colors(img);
+
+                    const char* output_path = "foo.png";
+
+                    save_image(img, output_path);
+                    printf("Preprocessed image saved at "
+                           "outputs/%s\n", output_path);
+
+                    free(img);
+                }
             }
-
-            grayscale(img);
-            apply_gaussian_blur(img);
-            increase_contrast(img, 10, 8);
-            binary(img);
-            otsu_threshold(img, 32);
-            invert_colors(img);
-            save_image(img, "outputs/preprocessed_image.png");
-            printf("Preprocessed image saved at outputs/preprocessed_image.png");
-
-            free(img);
             break;
         case 2:
             printf("You chose 'Manual Rotation'\n");
-
-            iImage *img = resize_image(load_image(argv[1], -1), 1000, 1000);
+            
+            iImage *img = resize_image(load_image(path_to_rotation, -1), 1000, 1000);
 
             if (img == NULL) {
                 printf("An error occured while loading the image");
@@ -82,26 +104,38 @@ int main() {
                 return EXIT_FAILURE;
             }
 
+            double angle;
+
+            printf("Please select an angle :\n");
+            scanf("%ld", &angle);
+
+            rotate_image(img, angle);
+
             save_image(img, "outputs/preprocessed_image.png");
-            printf("Rotated images saved at outputs/rotated_image_<i>.png");
+            printf("Rotated image saved at outputs/rotated_image_<i>.png");
             
             free(img);
             break;
-        case 3: 
-            // function that detects the grid
+        case 3: // not working rn
+            // for img in test_data:
+                // iImage *grid = extract_grid(img);
+                // save_image(grid, path);
             break;
-        case 4: 
-            // function that detects a word list
+        case 4:  // not working rn
+            // for img in test_data:
+                // iImage words_list = extract_words_list(img);
+                // save_image(grid, path)
             break;
-        case 5: 
-            // arrays containing words list of a specific grid
-            // funciton to extract words
+        case 5: // not working rn
+            // for img in test_data:
+                // iImage words = extract_words_list(img);
+                // save_image(grid, path)
             break;
-        case 6: 
+        case 6:  // not working rn
             // arrays containing each words of a specific word list and grids
             // function to extract letters
-        case 7: 
-            // XNOR();
+        case 7:  // done, works fine
+            XNOR();
             break;
         case 8: 
             // char* word;
